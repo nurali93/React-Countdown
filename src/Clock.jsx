b926d341e0b599b23046b8067e3b9b097d655f2a
nurali93 <nurali2293@gmail.com>
2017-07-17 11:43:07 +0800

    init commit

---
+++ src/Clock.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
+  }
+
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const hours = Math.floor(time / (1000 * 60 * 60) % 60) % 24;
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const weeks = Math.floor(time / (1000 * 60 * 60 * 24) / 7);
+
+    this.setState({weeks, days, hours, minutes, seconds});
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className = 'Clock-weeks d-inline'>{this.leading0(this.state.weeks)} weeks</div>
+        <div className = 'Clock-days d-inline'>{this.leading0(this.state.days)} days</div>
+        <div className = 'Clock-hours d-inline'>{this.leading0(this.state.hours)} hours</div>
+        <div className = 'Clock-minutes d-inline'>{this.leading0(this.state.minutes)} minutes</div>
+        <div className = 'Clock-seconds d-inline'>{this.leading0(this.state.seconds)} seconds</div>
+      </div>
+    )
+  }
+}
+
+export default Clock;
